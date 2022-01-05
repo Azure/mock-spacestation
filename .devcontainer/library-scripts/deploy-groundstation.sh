7dac2dfbbdfb4a22cc094885b5bdaae57acf5c97
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2022-01-05 18:12:15 +0000

    working dev container, not working connectoin

--- .devcontainer/library-scripts/deploy-groundstation.sh
+++ .devcontainer/library-scripts/deploy-groundstation.sh
@@ -32,7 +32,7 @@ export GROUNDSTATION_ROOTDIR="/groundstation"
 export GROUNDSTATION_LOGS_DIR="${GROUNDSTATION_ROOTDIR}/logs"
 export GROUNDSTATION_OUTBOX_DIR="${GROUNDSTATION_ROOTDIR}/toSpaceStation"
 export GROUNDSTATION_INBOX_DIR="${GROUNDSTATION_ROOTDIR}/fromSpaceStation"
-export GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH="/usr/local/bin/docker-in-docker"
+export GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH="/tmp/docker-in-docker"
 export GROUNDSTATION_SSHKEY_FILEPATH="${HOME}/.ssh/id_rsa_spacestation"
 export GROUNDSTATION_SSH_SCRIPT_FILENAME="ssh-to-spacestation.sh"
 export GROUNDSTATION_CRON_JOB_FILENAME="sync-to-spacestation.sh"
@@ -42,16 +42,9 @@ export SPACESTATION_NETWORK_NAME="spaceDevVNet"
 export SPACESTATION_DOCKERFILE_PATH="/tmp/Dockerfile.Spacestation"
 export SPACESTATION_CONTAINER_NAME="mockspacestation"
 export SPACESTATION_IMAGE_NAME="${SPACESTATION_CONTAINER_NAME}-img"
-export SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH="/usr/local/bin/docker-from-docker"
+export SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH="/tmp/docker-from-docker"
 
 export PROVISIONING_LOG="${GROUNDSTATION_LOGS_DIR}/deploy-groundstation.log"
-
-# check user
-
-if [[ $(whoami) -ne $GROUNDSTATION_USER ]]; then
-	echo "Please rerun this script as user '$GROUNDSTATION_USER'."
-	exit 1
-fi
 
 # setup logging
 
@@ -65,9 +58,10 @@ writeToProvisioningLog() {
 sudo mkdir -p "$GROUNDSTATION_LOGS_DIR"
 sudo mkdir -p "$GROUNDSTATION_OUTBOX_DIR"
 sudo mkdir -p "$GROUNDSTATION_INBOX_DIR"
-sudo mkdir -p "$HOME"/.ssh
 sudo mkdir -p "$GROUNDSTATION_ROOTDIR"
 sudo chown -R "$GROUNDSTATION_USER" "$GROUNDSTATION_ROOTDIR"
+
+sudo mkdir -p "$HOME"/.ssh
 
 # start logging
 
@@ -102,35 +96,21 @@ writeToProvisioningLog "PACKAGES: updated!"
 
 # setup docker
 
+set +e
+
 writeToProvisioningLog "DOCKER: setting up..."
 
 writeToProvisioningLog "check for docker..."
-if ! command -v docker &> /dev/null; then
+if type docker > /dev/null 2>&1; then
 	writeToProvisioningLog "installing Docker..."
 	
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	sudo echo \
 		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-	sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+	sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io >> /dev/null # TODO (20220103 gmusa): replace with debug file logging
 
 	writeToProvisioningLog "Docker installed!"
-fi
-
-writeToProvisioningLog "check if Remote Container..."
-if printenv | grep -q "REMOTE_CONTAINER" || printenv | grep -q "CODESPACE_NAME"; then
-	writeToProvisioningLog "this is a Remote Container, enabling Docker in Docker..."
-
-	writeToProvisioningLog "Downloading Docker-in-Docker script from '${docker_in_docker_script_uri}' to '$GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH'..."
-	curl -fsSL "${docker_in_docker_script_uri}" -o "${GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH}"
-	chmod +x "${GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH}"
-	writeToProvisioningLog "Docker-in-Docker downloaded!"
-
-	writeToProvisioningLog "Executing Docker-in-Docker script at '$GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH'..."
-	. "$GROUNDSTATION_DOCKER_IN_DOCKER_SCRIPT_FILEPATH"
-	writeToProvisioningLog "Docker-in-Docker script executed!"
-
-	writeToProvisioningLog "Docker-in-Docker enabled for Remote Container!"
 fi
 
 writeToProvisioningLog "adding user '${GROUNDSTATION_USER}' to docker group..."
@@ -159,19 +139,22 @@ writeToProvisioningLog "network '${SPACESTATION_NETWORK_NAME}' created"
 
 writeToProvisioningLog "DOCKER: setup complete!"
 
+set -e
+
 # generate SSH keys
 
 writeToProvisioningLog "SSH KEYS: generating groundstation keys..."
 
 writeToProvisioningLog "writing a rsa key to '${GROUNDSTATION_SSHKEY_FILEPATH}'..."
-sudo chmod 0700 "$HOME"/.ssh
-ssh-keygen -t rsa -b 4096 -q -N '' -f "${GROUNDSTATION_SSHKEY_FILEPATH}" <<<y
+sudo chmod 700 "$HOME"/.ssh
+sudo ssh-keygen -t rsa -b 4096 -q -N '' -f "${GROUNDSTATION_SSHKEY_FILEPATH}"
 writeToProvisioningLog "rsa key written to '${GROUNDSTATION_SSHKEY_FILEPATH}'!"
 
 writeToProvisioningLog "adding rsa key at '${GROUNDSTATION_SSHKEY_FILEPATH}' to authorized_keys..."
-sudo chmod 600 "$GROUNDSTATION_SSHKEY_FILEPATH" &&
-	sudo chmod 600 "$GROUNDSTATION_SSHKEY_FILEPATH".pub &&
-	cat "${GROUNDSTATION_SSHKEY_FILEPATH}".pub >>/home/"${GROUNDSTATION_USER}"/.ssh/authorized_keys
+sudo chmod 600 "${GROUNDSTATION_SSHKEY_FILEPATH}"
+sudo chmod 600 "${GROUNDSTATION_SSHKEY_FILEPATH}".pub
+sudo chmod 777 "$HOME"/.ssh
+sudo cat "${GROUNDSTATION_SSHKEY_FILEPATH}.pub" | tee -a "/home/${GROUNDSTATION_USER}/.ssh/authorized_keys"
 writeToProvisioningLog "rsa key added to authorized_keys!"
 
 writeToProvisioningLog "SSH KEYS: groundstation keys generated!"
@@ -186,19 +169,20 @@ writeToProvisioningLog "spacestation Dockerfile downloaded!"
 
 writeToProvisioningLog "changing Dockerfile at '${SPACESTATION_DOCKERFILE_PATH}' ownership to user '${GROUNDSTATION_USER}'..."
 sudo chown "$GROUNDSTATION_USER" "${SPACESTATION_DOCKERFILE_PATH}"
-sudo chmod 1777 "${SPACESTATION_DOCKERFILE_PATH}"
+sudo chmod 777 "${SPACESTATION_DOCKERFILE_PATH}"
 writeToProvisioningLog "Dockerfile ownership updated!"
 
 writeToProvisioningLog "building image '${SPACESTATION_IMAGE_NAME}' from '${SPACESTATION_DOCKERFILE_PATH}'..."
 sudo docker build -t "${SPACESTATION_IMAGE_NAME}" \
 	--no-cache \
 	--build-arg SPACESTATION_USER="$GROUNDSTATION_USER" \
-	--build-arg PRIV_KEY="$(cat "${GROUNDSTATION_SSHKEY_FILEPATH}")" \
-	--build-arg PUB_KEY="$(cat "${GROUNDSTATION_SSHKEY_FILEPATH}".pub)" \
+	--build-arg PRIV_KEY="$(sudo cat "${GROUNDSTATION_SSHKEY_FILEPATH}")" \
+	--build-arg PUB_KEY="$(sudo cat "${GROUNDSTATION_SSHKEY_FILEPATH}".pub)" \
 	--build-arg DOCKER_FROM_DOCKER_SCRIPT_URI="${docker_from_docker_script_uri}" \
-	--build-arg DOCKER_FROM_DOCKER_SCRIPT_PATH="${SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}" \
+	--build-arg DOCKER_FROM_DOCKER_SCRIPT_FILEPATH="${SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}" \
 	--file "${SPACESTATION_DOCKERFILE_PATH}" . \
 	>>/dev/null # TODO (20220103 gmusa): replace with debug file logging
+sudo chmod 700 "$HOME"/.ssh
 writeToProvisioningLog "image built!"
 
 writeToProvisioningLog "BUILD IMAGE: spacestation image '${SPACESTATION_IMAGE_NAME}' built!"
@@ -215,16 +199,6 @@ sudo docker run -dit \
 	--network "${SPACESTATION_NETWORK_NAME}" \
 	"${SPACESTATION_IMAGE_NAME}"
 writeToProvisioningLog "spacestation container running!"
-
-writeToProvisioningLog "downloading Docker-from-Docker script from '${docker_from_docker_script_uri}' to '$GROUNDSTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH'..."
-curl -fsSL "${docker_from_docker_script_uri}" -o "${GROUNDSTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}"
-chmod +x "${GROUNDSTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}"
-writeToProvisioningLog "Docker-from-Docker script downloaded!"
-
-writeToProvisioningLog "execute docker-from-docker script at '${SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}' on '${SPACESTATION_CONTAINER_NAME}'"
-sudo docker exec -ti "${SPACESTATION_CONTAINER_NAME}" \
-	bash -c "${SPACESTATION_DOCKER_FROM_DOCKER_SCRIPT_FILEPATH}"
-writeToProvisioningLog "docker-from-docker script executed!"
 
 writeToProvisioningLog "set docker acl on '${SPACESTATION_CONTAINER_NAME}' for user '${GROUNDSTATION_USER}'"
 sudo docker exec -ti "${SPACESTATION_CONTAINER_NAME}" \
@@ -261,20 +235,24 @@ mockspacestation_ip=$(sudo docker inspect ${SPACESTATION_CONTAINER_NAME} --forma
 writeToProvisioningLog "spacestation container IP address '${mockspacestation_ip}' retrieved"
 
 writeToProvisioningLog "updating spacestation known hosts for passwordless SSH to groundstation"
-groundstation_hostname=$(hostname)
+writeToProvisioningLog "hostname: $(hostname)"
+writeToProvisioningLog "ip address: $(hostname -I)"
+groundstation_hostname=$(hostname -I)
 sudo docker exec -ti "${SPACESTATION_CONTAINER_NAME}" \
-	bash -c "ssh-keyscan ${groundstation_hostname} >> /home/${GROUNDSTATION_USER}/.ssh/known_hosts"
+	bash -c "ssh-keyscan -p 2222 ${groundstation_hostname} >> /home/${GROUNDSTATION_USER}/.ssh/known_hosts" # TODO (20210103 gmusa): remove hardcoded port
 writeToProvisioningLog "groundstation added to spacestation known hosts!"
 
 writeToProvisioningLog "updating groundstation known hosts for passwordless SSH to spacestation"
+sudo chmod 777 "${HOME}/.ssh"
 ssh-keyscan "${mockspacestation_ip}" >> "${HOME}/.ssh/known_hosts"
+sudo chmod 700 "${HOME}/.ssh"
 writeToProvisioningLog "spacestation added to groundstation known hosts!"
 
 writeToProvisioningLog "writing SSH connection script to '${SPACESTATION_CONTAINER_NAME}'..."
-{
-	echo "#!/bin/bash"
-	echo "ssh -i \"${GROUNDSTATION_SSHKEY_FILEPATH}\" \"${GROUNDSTATION_USER}\"@\"${mockspacestation_ip}\""
-} >${GROUNDSTATION_ROOTDIR}/${GROUNDSTATION_SSH_SCRIPT_FILENAME}
+cat > ${GROUNDSTATION_ROOTDIR}/${GROUNDSTATION_SSH_SCRIPT_FILENAME} << EOL
+#!/bin/bash
+ssh -i ${GROUNDSTATION_SSHKEY_FILEPATH} ${GROUNDSTATION_USER}@${mockspacestation_ip}
+EOL
 sudo chmod 777 ${GROUNDSTATION_ROOTDIR}/${GROUNDSTATION_SSH_SCRIPT_FILENAME}
 writeToProvisioningLog "SSH connection script '${GROUNDSTATION_ROOTDIR}/${GROUNDSTATION_SSH_SCRIPT_FILENAME}' written!"
 
@@ -312,21 +290,22 @@ writeToProvisioningLog "cron job scheduled!"
 # Build SSH and RSYNC Jobs: END
 # ********************************************************
 
-clear
-
-figlet Azure Mock SpaceStation
+figlet Azure Mock Spacestation
 
 echo ""
 echo ""
-echo "Welcome to the Mock SpaceStation Template (https://github.com/azure/mock-spacestation) v2.1!"
+echo "Welcome to the Mock Spacestation (https://github.com/azure/mock-spacestation) v2.1!"
 echo ""
 echo "This environment emulates the configuration and networking constraints observed by the Microsoft Azure Space Team while developing their genomics experiment to run on the International Space Station."
 echo ""
-echo "You are connected to the GroundStation"
-echo "     To send a file to the SpaceStation, place it in the '$GROUNDSTATION_OUTBOX_DIR' directory"
-echo "     Files received from the SpaceStation will be in the '$GROUNDSTATION_INBOX_DIR' directory"
-echo "To SSH to SpaceStation: '${GROUNDSTATION_ROOTDIR}/ssh-to-spacestation.sh'"
-echo "     Files received from the GroundStation will be in '/home/$GROUNDSTATION_USER/fromGroundStation/' directory"
-echo "     To send a file to the GroundStation, place it in the '/home/$GROUNDSTATION_USER/toGroundStation/' directory"
+echo "You are connected to the Groundstation:"
+echo "     - to send a file to the Spacestation, place it in the '$GROUNDSTATION_OUTBOX_DIR' directory"
+echo "     - incoming files from the Spacestation will be in the '$GROUNDSTATION_INBOX_DIR' directory"
+echo ""
+echo "To connect to the Spacestation: '${GROUNDSTATION_ROOTDIR}/ssh-to-spacestation.sh'"
+echo "     - files received from the Groundstation will be in '/home/$GROUNDSTATION_USER/fromGroundStation/' directory"
+echo "     - to send a file to the Groundstation, place it in the '/home/$GROUNDSTATION_USER/toGroundStation/' directory"
+echo ""
 echo "Happy Space Deving!"
+echo ""
 cd "$GROUNDSTATION_ROOTDIR" || exit
